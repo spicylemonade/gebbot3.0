0ec5ca6ada2eafad0be1388d8b0b23dafd2fe397
spicylemonade <84095175+spicylemonade@users.noreply.github.com>
2021-12-11 23:07:23 -0500

    more

--- main.py
+++ main.py
@@ -558,7 +558,7 @@ async def dm(ctx, guild_id: int):
     await ctx.author.send(invitelink)
        
 @client.command()
-async def bank(ctx, user:str or discord.Member=None):
+async def bank(ctx, user:discord.Member or str=None):
         try:
             if(user == None):
                     member = ctx.message.author
