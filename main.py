98037c69b37a0192d6d62abc65d44629aec60e69
spicylemonade <84095175+spicylemonade@users.noreply.github.com>
2021-12-19 21:33:02 -0500

    education

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import chart_studio
 import discord
 from discord.ext import commands
 import random
@@ -684,40 +683,6 @@ async def work(ctx):
             embedi = discord.Embed(title=f":necktie: {t}", description="you gained: $"+ str(b), color=(0x25be2a))
             await ctx.send(embed=embedi)
 
-
-@client.command()
-async def learn(ctx):
-        mip = "'"
-        await update_name(str(ctx.author.id))
-        my_cursor.execute(f"SELECT edu_var FROM geb_economy WHERE discord_id = {mip + str(ctx.author.id) + mip}")
-        x=my_cursor.fetchone()
-        if x[0] >= 10:
-            embedi = discord.Embed(description="you can no learn for today")
-            await ctx.send(embed=embedi)
-        else:
-            my_cursor.execute(f"SELECT education FROM geb_economy WHERE discord_id = {mip + str(ctx.author.id) + mip}")
-            for m in my_cursor:
-                m=functools.reduce(operator.add, (m))
-            await edu_list(m) #used to select howmuch to subtract yourmoney based on what your major is
-            b =y_edu #y_edu is the value
-            await update_data(str(ctx.author.id),b)
-            my_cursor.execute(f"UPDATE geb_economy SET edu_var=edu_var+1 WHERE discord_id = {mip + str(ctx.author.id) + mip}")
-            my_cursor.execute(f"UPDATE geb_economy SET total_edu_var=total_edu_var+1 WHERE discord_id = {mip + str(ctx.author.id) + mip}")
-            mydb.commit()
-            my_cursor.execute(f"SELECT education FROM geb_economy WHERE discord_id = {mip + str(ctx.author.id) + mip}")
-            for major_name in my_cursor:
-                major_name=functools.reduce(operator.add, (major_name))
-                #major_name is the major you currently have
-            embedi = discord.Embed(title=f":mortar_board: {major_name}", description="tuition cost: $"+ str(b), color=(0x25be2a))
-            await ctx.send(embed=embedi)
-            my_cursor.execute(f"SELECT total_edu_var FROM geb_economy WHERE discord_id = {mip + str(ctx.author.id) + mip}")
-            for v in my_cursor:
-                v=functools.reduce(operator.add, (v))
-                #v is how many times youve learned or said g!learn in that specific major
-            if v== 100:
-                my_cursor.execute(f"UPDATE geb_economy SET total_list=total_list+',{major_name}' WHERE discord_id = {mip + str(ctx.author.id) + mip}")
-                embedi = discord.Embed(title=f":mortar_board: {major_name}", description=f"you have finished your {major_name} major" , color=(0x25be2a))
-                await ctx.send(embed=embedi)
                   
                  
 @client.command()
@@ -726,7 +691,7 @@ async def give(ctx,amount,*, user: discord.Member):
         x = my_cursor.fetchone()
         #x is just how much money you have
         if int(amount) > x[0]:
-            embedi = discord.Embed(title="gammble", description="you dont own that much")
+            embedi = discord.Embed(title="gamble", description="you dont own that much")
             await ctx.send(embed=embedi)
         else:
          await update_give(str(ctx.author.id),user,int(amount))
@@ -753,13 +718,13 @@ async def rank(ctx):
                 #b1= b.replace("Decimal", "")
                 #b1= b1.replace("("," ")
                 #b1 = b1.replace(")", " ")
-                b1 = str(b)+"                              "
+                b1 = str(b)+""
                 #b1 = b.translate(None, 'decimal')
-                c1 = str(c)+"                              "
+                c1 = str(c)+""
                 try:
-                    d1 = str(d)+"                              "
+                    d1 = str(d)+""
                 except:
-                    d1= str(d)+"                              "
+                    d1= str(d)+" "
                 embedi.add_field(name=member.name,
                      value=b1+c1+d1,
                      inline=True)
@@ -834,8 +799,224 @@ async def gamble(ctx, choice, amount):
                      await update_gamble(ctx.author.id,choice,int(int(x)/2))
                      embedi = discord.Embed(title="gamble :game_die::game_die:", description =aft, color=(col_val))
                      await ctx.send(embed=embedi)
-            
-                  
+@client.command()
+async def learn(ctx):
+    try:
+        mip = "'"
+        await update_name(str(ctx.author.id))
+        my_cursor.execute(f"SELECT edu_var FROM geb_economy WHERE discord_id = {mip + str(ctx.author.id) + mip}")
+        x=my_cursor.fetchone()
+        if x[0] >= 10:
+            embedi = discord.Embed(description="you can no longer learn for today")
+            await ctx.send(embed=embedi)
+        else:
+            my_cursor.execute(f"SELECT education FROM geb_economy WHERE discord_id = {mip + str(ctx.author.id) + mip}")
+            for m in my_cursor:
+                m=functools.reduce(operator.add, (m))
+            await edu_list(m)
+            b =y_edu
+            my_cursor.execute(f"SELECT money FROM geb_economy WHERE discord_id = {mip + str(ctx.author.id) + mip}")
+            for m in my_cursor:
+                m=functools.reduce(operator.add, (m))
+            if (y_edu*-1) > m:
+                embedi = discord.Embed(title=f":mortar_board:", description="not enough money to pay for tuition", color=(0xFFD700))
+                await ctx.send(embed=embedi)
+            else:
+                await update_data(str(ctx.author.id),b)
+                my_cursor.execute(f"UPDATE geb_economy SET edu_var=edu_var+1 WHERE discord_id = {mip + str(ctx.author.id) + mip}")
+                my_cursor.execute(f"UPDATE geb_economy SET total_edu_var=total_edu_var+1 WHERE discord_id = {mip + str(ctx.author.id) + mip}")
+                mydb.commit()
+                my_cursor.execute(f"SELECT education FROM geb_economy WHERE discord_id = {mip + str(ctx.author.id) + mip}")
+                for edu_name in my_cursor:
+                    edu_name=functools.reduce(operator.add, (edu_name))
+                embedi = discord.Embed(title=f":mortar_board: {edu_name}", description="you lost: $"+ str(b), color=(0xFFD700))
+                await ctx.send(embed=embedi)
+                my_cursor.execute(f"SELECT total_edu_var FROM geb_economy WHERE discord_id = {mip + str(ctx.author.id) + mip}")
+                for t in my_cursor:
+                    t=functools.reduce(operator.add, (t))
+                if t == 50:
+                    my_cursor.execute(f"UPDATE geb_economy SET edu_list = CONCAT(edu_list, '_{edu_name}') WHERE discord_id = {mip + str(ctx.author.id) + mip}")
+                    embedi = discord.Embed(title=f":mortar_board: {edu_name}", description="congrats, you graduated", color=(0xFFD700))
+                    await ctx.send(embed=embedi)
+                    my_cursor.execute(f"UPDATE geb_economy SET education = NULL WHERE discord_id = {mip + str(ctx.author.id) + mip}")
+                    my_cursor.execute(f"UPDATE geb_economy SET total_edu_var = 0 WHERE discord_id = {mip + str(ctx.author.id) + mip}")
+                    mydb.commit()
+    except:
+        embedi = discord.Embed(title=f":mortar_board:", description="choose a major first {g!edu}", color=(0xFFD700))
+        await ctx.send(embed=embedi)
+
+@client.command()
+async def major(ctx, edun):
+    embedi = discord.Embed(title=major,description=':mortar_board: you are now majoring', color=(0xFFD700))
+    if int(edun) == 1:
+        my_cursor.execute(f"SELECT edu_list FROM geb_economy WHERE discord_id = {mip + str(ctx.author.id) + mip}")
+        for edu_list in my_cursor:
+            edu_list=functools.reduce(operator.add, (edu_list))
+            if not 'english' in edu_list:
+                my_cursor.execute(f"UPDATE geb_economy SET education = 'english' WHERE discord_id = {mip + str(ctx.author.id) + mip}")
+                my_cursor.execute(f"UPDATE geb_economy SET total_edu_var = 0 WHERE discord_id = {mip + str(ctx.author.id) + mip}")
+                await ctx.send(embed=embedi)
+            else:
+                embedi = discord.Embed(title=major,description=':mortar_board: you already own a degree in this, try a different major', color=(0xFFD700))
+                await ctx.send(embed=embedi)
+    if int(edun) == 2:
+        my_cursor.execute(f"SELECT edu_list FROM geb_economy WHERE discord_id = {mip + str(ctx.author.id) + mip}")
+        for edu_list in my_cursor:
+            edu_list=functools.reduce(operator.add, (edu_list))
+            if not 'food' in edu_list:
+                my_cursor.execute(f"UPDATE geb_economy SET education = 'food' WHERE discord_id = {mip + str(ctx.author.id) + mip}")
+                my_cursor.execute(f"UPDATE geb_economy SET total_edu_var = 0 WHERE discord_id = {mip + str(ctx.author.id) + mip}")
+                await ctx.send(embed=embedi)
+            else:
+                embedi = discord.Embed(title=major,description=':mortar_board: you already own a degree in this, try a different major', color=(0xFFD700))
+                await ctx.send(embed=embedi)
+    if int(edun) == 3:
+        my_cursor.execute(f"SELECT edu_list FROM geb_economy WHERE discord_id = {mip + str(ctx.author.id) + mip}")
+        for edu_list in my_cursor:
+            edu_list=functools.reduce(operator.add, (edu_list))
+            if not 'art' in edu_list:
+                my_cursor.execute(f"UPDATE geb_economy SET education = 'art' WHERE discord_id = {mip + str(ctx.author.id) + mip}")
+                my_cursor.execute(f"UPDATE geb_economy SET total_edu_var = 0 WHERE discord_id = {mip + str(ctx.author.id) + mip}")
+                await ctx.send(embed=embedi)
+            else:
+                embedi = discord.Embed(title=major,description=':mortar_board: you already own a degree in this, try a different major', color=(0xFFD700))
+                await ctx.send(embed=embedi)
+    if int(edun) == 4:
+        my_cursor.execute(f"SELECT edu_list FROM geb_economy WHERE discord_id = {mip + str(ctx.author.id) + mip}")
+        for edu_list in my_cursor:
+            edu_list=functools.reduce(operator.add, (edu_list))
+            if not 'chem' in edu_list:
+                my_cursor.execute(f"UPDATE geb_economy SET education = 'chem' WHERE discord_id = {mip + str(ctx.author.id) + mip}")
+                my_cursor.execute(f"UPDATE geb_economy SET total_edu_var = 0 WHERE discord_id = {mip + str(ctx.author.id) + mip}")
+                await ctx.send(embed=embedi)
+            else:
+                embedi = discord.Embed(title=major,description=':mortar_board: you already own a degree in this, try a different major', color=(0xFFD700))
+                await ctx.send(embed=embedi)
+    if int(edun) == 5:
+        my_cursor.execute(f"SELECT edu_list FROM geb_economy WHERE discord_id = {mip + str(ctx.author.id) + mip}")
+        for edu_list in my_cursor:
+            edu_list=functools.reduce(operator.add, (edu_list))
+            if not 'health' in edu_list:
+                my_cursor.execute(f"UPDATE geb_economy SET education = 'health' WHERE discord_id = {mip + str(ctx.author.id) + mip}")
+                my_cursor.execute(f"UPDATE geb_economy SET total_edu_var = 0 WHERE discord_id = {mip + str(ctx.author.id) + mip}")
+                await ctx.send(embed=embedi)
+            else:
+                embedi = discord.Embed(title=major,description=':mortar_board: you already own a degree in this, try a different major', color=(0xFFD700))
+                await ctx.send(embed=embedi)
+    if int(edun) == 6:
+        my_cursor.execute(f"SELECT edu_list FROM geb_economy WHERE discord_id = {mip + str(ctx.author.id) + mip}")
+        for edu_list in my_cursor:
+            edu_list=functools.reduce(operator.add, (edu_list))
+            if not 'compsci' in edu_list:
+                my_cursor.execute(f"UPDATE geb_economy SET education = 'compsci' WHERE discord_id = {mip + str(ctx.author.id) + mip}")
+                my_cursor.execute(f"UPDATE geb_economy SET total_edu_var = 0 WHERE discord_id = {mip + str(ctx.author.id) + mip}")
+                await ctx.send(embed=embedi)
+            else:
+                embedi = discord.Embed(title=major,description=':mortar_board: you already own a degree in this, try a different major', color=(0xFFD700))
+                await ctx.send(embed=embedi)
+    if int(edun) == 7:
+        my_cursor.execute(f"SELECT edu_list FROM geb_economy WHERE discord_id = {mip + str(ctx.author.id) + mip}")
+        for edu_list in my_cursor:
+            edu_list=functools.reduce(operator.add, (edu_list))
+            if not 'politics' in edu_list:
+                my_cursor.execute(f"UPDATE geb_economy SET education = 'politics' WHERE discord_id = {mip + str(ctx.author.id) + mip}")
+                my_cursor.execute(f"UPDATE geb_economy SET total_edu_var = 0 WHERE discord_id = {mip + str(ctx.author.id) + mip}")
+                await ctx.send(embed=embedi)
+            else:
+                embedi = discord.Embed(title=major,description=':mortar_board: you already own a degree in this, try a different major', color=(0xFFD700))
+                await ctx.send(embed=embedi)
+    if int(edun) == 8:
+        my_cursor.execute(f"SELECT edu_list FROM geb_economy WHERE discord_id = {mip + str(ctx.author.id) + mip}")
+        for edu_list in my_cursor:
+            edu_list=functools.reduce(operator.add, (edu_list))
+            if not 'business' in edu_list:
+                my_cursor.execute(f"UPDATE geb_economy SET education = 'business' WHERE discord_id = {mip + str(ctx.author.id) + mip}")
+                my_cursor.execute(f"UPDATE geb_economy SET total_edu_var = 0 WHERE discord_id = {mip + str(ctx.author.id) + mip}")
+                await ctx.send(embed=embedi)
+                
+            else:
+                embedi = discord.Embed(title=major,description=':mortar_board: you already own a degree in this, try a different major', color=(0xFFD700))
+                await ctx.send(embed=embedi)
+    mydb.commit()
+        
+
+
+@client.command()
+async def edu(ctx):
+    embedi = discord.Embed(title=':mortar_board: Education', color=(0xFFD700))
+    embedi.add_field(name="english(1)",
+                     value="max tuition pay $150",
+                     inline=True)
+    embedi.add_field(name="food(2)",
+                     value="max tuition pay $300",
+                     inline=True)
+    embedi.add_field(name="art(3)",
+                     value="max tuition pay $450",
+                     inline=True)
+    embedi.add_field(name="chem(4)",
+                     value="max tuition pay $600",
+                     inline=True)
+    embedi.add_field(name="health(5)",
+                     value="max tuition pay $750",
+                     inline=True)
+    embedi.add_field(name="compsci(6)",
+                     value="max tuition pay $900",
+                     inline=True)
+    embedi.add_field(name="politics(7)",
+                     value="max tuition pay $1050",
+                     inline=True)
+    embedi.add_field(name="business(8)",
+                     value="max tuition pay $1200",
+                     inline=True)
+    embedi.add_field(name=":envelope_with_arrow: command",
+                     value="g!major {number}",
+                     inline=False)
+    await ctx.send(embed=embedi)
+@client.command()
+async def economy(ctx):
+    embedi = discord.Embed(title=':yen: geb_economy', color=(0xFFD700))
+    embedi.add_field(name="g!bank",
+                     value="checks your balance, add player name after to check specific player(adds you into the database)",
+                     inline=True)
+    embedi.add_field(name="g!gamble {heads or tails} {number}",
+                     value="gamble a specified amount, luck based. Replace number with all or half to gamble all money or half",
+                     inline=True)
+    embedi.add_field(name="g!work",
+                     value="you start off with a specific job, that job determins the max amount of money you earn using this command (max amount of time you can work a day is 20, can increase with purchases)",
+                     inline=True)
+    embedi.add_field(name="g!rob {player}",
+                     value="rob a player, rates drop if you have robbed more than 7 times that day already and if robbie is online(max rob is 20, increasable with purchases)",
+                     inline=True)
+    embedi.add_field(name="g!give {amount} {player}",
+                     value="withdrawals specified amount of money from your bank and gives it to specified player",
+                     inline=True)
+    embedi.add_field(name="g!rank",
+                     value="check player rankings",
+                     inline=True)
+    embedi.add_field(name="g!edu",
+                     value="shows the available majors and their tuition pay per use",
+                     inline=True)
+    embedi.add_field(name="g!major {major number}",
+                     value="picks what you want to major in ex. 1=english",
+                     inline=True)
+    embedi.add_field(name="g!learn",
+                     value="allows you to learn in the major you are in at the moment, subtracts the tuition from your money. You must learn a specific maor 50 times to earn a degree(max learn a day is 5 can be increased)",
+                     inline=True)
+    embedi.add_field(name="g!edu_list",
+                     value="shows list of your degrees",
+                     inline=True)
+    embedi.add_field(name="g!edu_info",
+                     value="more in depth explanation on what majors do",
+                     inline=True)
+    await ctx.send(embed=embedi)
+@client.command()
+async def edu_list(ctx):
+    my_cursor.execute(f"SELECT edu_list FROM geb_economy WHERE discord_id = {mip + str(ctx.author.id) + mip}")
+    for edu_list in my_cursor:
+        edu_list=functools.reduce(operator.add, (edu_list))
+    embedi = discord.Embed(title=':mortar_board: your degrees',description=edu_list, color=(0xFFD700))
+    await ctx.send(embed=embedi)
+     
 async def update_name(ctxy):
     my_cursor.execute("SELECT * FROM geb_economy;")
     mip = "'"
@@ -913,44 +1094,31 @@ async def job_list(m):
         y_job=85
     elif m == 'construction_worker':
         y_job=90
-    elif m == 'english_major':
-        y_job=-90
-    elif m == 'food_major':
-        y_job=-110
-    elif m == 'art_major':
-        y_job=-140
-    elif m == 'chem_major':
-        y_job=-160
-    elif m == 'compsci_major':
-        y_job=-180
-    elif m == 'politics_major':
-        y_job=-250
-    elif m == 'bussiness_major':
-        y_job=-300
 #all of the jobs
 async def edu_list(m):
     global y_edu
-    if m == 'english_major':
-        y_edu=-90
-    elif m == 'food_major':
-        y_edu=-110
-    elif m == 'art_major':
-        y_edu=-140
-    elif m == 'chem_major':
-        y_edu=-160
-    elif m== 'health_major':
-        y_edu=-180
-    elif m == 'compsci_major':
-        y_edu=-250
-    elif m == 'politics_major':
+    if m == 'english':
+        y_edu=-150
+    elif m == 'food':
         y_edu=-300
-    elif m == 'bussiness_major':
-        y_edu=-400
+    elif m == 'art':
+        y_edu=-450
+    elif m == 'chem':
+        y_edu=-600
+    elif m== 'health':
+        y_edu=-750
+    elif m == 'compsci':
+        y_edu=-900
+    elif m == 'politics':
+        y_edu=-1050
+    elif m == 'bussiness':
+        y_edu=-1200
 #all of the  majors(they subtract money)
                 
 
 async def m_loop():
          while True:
+
                   global mydb
                   global my_cursor
                   mydb = mysql.connector.connect(host="bdrpelbcfmnvbfxgeoe6-mysql.services.clever-cloud.com", user="uhiollzjpdbggq7z",passwd="ETZYMs1wQWWGA1Vnq590",database="bdrpelbcfmnvbfxgeoe6",port=3306)
